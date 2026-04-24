d8092e9432ad634d8bb3d3c30d7c4bd80e6dfcfa
Howon Lee <kck4156@kagamine-rin.com>
2026-04-25 02:46:57 +0900

    Prioritize single-instance redirect over admin relaunch

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using DeskBorder.Helpers;
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Xaml;
 using Microsoft.Windows.AppLifecycle;
+using System.Linq;
 using System.Threading;
 
 namespace DeskBorder;
@@ -15,11 +16,14 @@ public static class Program
     {
         WinRT.ComWrappersSupport.InitializeComWrappers();
         var currentAppActivationArguments = AppInstance.GetCurrent().GetActivatedEventArgs();
+        if (TryRedirectToExistingRegisteredInstance(currentAppActivationArguments))
+            return;
+
         if (StartupRegistrationHelper.ShouldTryLaunchAsAdministratorFromStoredSettings(currentAppActivationArguments)
             && StartupRegistrationHelper.TryLaunchAsAdministratorAsync().GetAwaiter().GetResult())
             return;
 
-        if (TryRedirectToExistingInstance())
+        if (TryRegisterOrRedirectToMainInstance(currentAppActivationArguments))
             return;
 
         Application.Start(applicationInitializationCallbackParameters =>
@@ -31,7 +35,17 @@ public static class Program
         });
     }
 
-    private static bool TryRedirectToExistingInstance()
+    private static bool TryRedirectToExistingRegisteredInstance(AppActivationArguments currentAppActivationArguments)
+    {
+        var existingMainInstance = AppInstance.GetInstances().FirstOrDefault(appInstance => string.Equals(appInstance.Key, SingleInstanceKey, StringComparison.Ordinal));
+        if (existingMainInstance is null)
+            return false;
+
+        existingMainInstance.RedirectActivationToAsync(currentAppActivationArguments).AsTask().GetAwaiter().GetResult();
+        return true;
+    }
+
+    private static bool TryRegisterOrRedirectToMainInstance(AppActivationArguments currentAppActivationArguments)
     {
         var mainInstance = AppInstance.FindOrRegisterForKey(SingleInstanceKey);
         if (mainInstance.IsCurrent)
@@ -40,8 +54,7 @@ public static class Program
             return false;
         }
 
-        var appActivationArguments = AppInstance.GetCurrent().GetActivatedEventArgs();
-        mainInstance.RedirectActivationToAsync(appActivationArguments).AsTask().GetAwaiter().GetResult();
+        mainInstance.RedirectActivationToAsync(currentAppActivationArguments).AsTask().GetAwaiter().GetResult();
         return true;
     }
 
